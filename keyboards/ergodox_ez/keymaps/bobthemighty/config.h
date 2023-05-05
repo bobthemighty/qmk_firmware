652da22e6e49474100f9e6576ce17f2db49cb653
Bob Gregory <bob@codefiend.co.uk>
2023-05-05 19:13:45 +0100

    Meh?

---
+++ keyboards/ergodox_ez/keymaps/bobthemighty/config.h
@@ -0,0 +1,16 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define AUTO_SHIFT_TIMEOUT 150
+#define ORYX_CONFIGURATOR
+#define IGNORE_MOD_TAP_INTERRUPT
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define NO_AUTO_SHIFT_SPECIAL
+#define FIRMWARE_VERSION u8"RWo0l/rNOXw"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
+
+
+#define PERMISSIVE_HOLD
